92008181f04af4a02f6f7db18b793824b4973589
Harshal <charshal12@yahoo.in>
2023-02-20 22:02:55 +0530

    Added Jackson mapper

---
+++ src/test/java/utils/JacksonObjectMapper.java
@@ -0,0 +1,26 @@
+package utils;
+
+import Modal.Team;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class JacksonObjectMapper {
+    public static final String TEST_INPUT_FOLDER = "inputJson";
+    public static final String JSON_EXTENSION = ".json";
+
+    public  Team getDataInObjectFromJson(String fileName) throws IOException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        String filePath = TEST_INPUT_FOLDER.concat("/").concat(fileName).concat(JSON_EXTENSION);
+        InputStream inputStream = classLoader.getResourceAsStream(filePath);
+
+        //create ObjectMapper instance
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        //convert json string to object
+        Team team = objectMapper.readValue(inputStream, Team.class);
+        /*System.out.println("Team Object : " + team.toString());*/
+        return team;
+    }
+}
